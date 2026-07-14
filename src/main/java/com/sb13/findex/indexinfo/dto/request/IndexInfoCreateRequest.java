a54e4d76b61762e7756deff0460cab09a21ac6c9
hajung <bover80@naver.com>
2026-07-14 13:50:02 +0900

    Add: Request에 toCommand() 추가 및 Controller 생성

--- src/main/java/com/sb13/findex/indexinfo/dto/request/IndexInfoCreateRequest.java
+++ src/main/java/com/sb13/findex/indexinfo/dto/request/IndexInfoCreateRequest.java
@@ -1,14 +1,33 @@
 package com.sb13.findex.indexinfo.dto.request;
 
+import com.sb13.findex.indexinfo.dto.command.*;
+import jakarta.validation.constraints.*;
+
 import java.math.*;
 import java.time.*;
 
 public record IndexInfoCreateRequest(
+        @NotBlank
         String indexClassification,
+        @NotBlank
         String indexName,
+        @Positive
         int employedItemsCount,
+        @NotNull
         LocalDate basePointInTime,
+        @Positive
+        @NotNull
         BigDecimal baseIndex,
         boolean favorite
 ) {
+    public IndexInfoCreateCommand toCommand() {
+        return new IndexInfoCreateCommand(
+                indexClassification,
+                indexName,
+                employedItemsCount,
+                basePointInTime,
+                baseIndex,
+                favorite
+        );
+    }
 }
--- src/main/java/com/sb13/findex/indexinfo/dto/request/IndexInfoUpdateRequest.java
+++ src/main/java/com/sb13/findex/indexinfo/dto/request/IndexInfoUpdateRequest.java
@@ -1,12 +1,25 @@
 package com.sb13.findex.indexinfo.dto.request;
 
+import com.sb13.findex.indexinfo.dto.command.*;
+import jakarta.validation.constraints.*;
+
 import java.math.*;
 import java.time.*;
 
 public record IndexInfoUpdateRequest(
+       @Positive
         Integer employedItemsCount,
         LocalDate basePointInTime,
+        @Positive
         BigDecimal baseIndex,
         Boolean favorite
 ) {
+    public IndexInfoUpdateCommand toCommand() {
+        return new IndexInfoUpdateCommand(
+                employedItemsCount,
+                basePointInTime,
+                baseIndex,
+                favorite
+        );
+    }
 }
