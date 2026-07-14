f29c5850adc6c857ca0aa0255a597f6c0c9b3c3a
DHK777 <kjckjcdo@gmail.com>
2026-07-14 14:16:38 +0900

    Fix: 음수 검증

--- src/main/java/com/sb13/findex/indexdata/dto/request/IndexDataUpdateRequest.java
+++ src/main/java/com/sb13/findex/indexdata/dto/request/IndexDataUpdateRequest.java
@@ -1,17 +1,19 @@
 package com.sb13.findex.indexdata.dto.request;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 
 public record IndexDataUpdateRequest(
-    @NotNull BigDecimal marketPrice,
-    @NotNull BigDecimal closingPrice,
-    @NotNull BigDecimal highPrice,
-    @NotNull BigDecimal lowPrice,
+    @NotNull @DecimalMin("0.0") BigDecimal marketPrice,
+    @NotNull @DecimalMin("0.0") BigDecimal closingPrice,
+    @NotNull @DecimalMin("0.0") BigDecimal highPrice,
+    @NotNull @DecimalMin("0.0") BigDecimal lowPrice,
     @NotNull BigDecimal versus,
     @NotNull BigDecimal fluctuationRate,
-    @NotNull Long tradingQuantity,
-    @NotNull Long tradingPrice,
-    @NotNull Long marketTotalAmount
+    @NotNull @PositiveOrZero Long tradingQuantity,
+    @NotNull @PositiveOrZero Long tradingPrice,
+    @NotNull @PositiveOrZero Long marketTotalAmount
 ) {
 }
